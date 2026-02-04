3f56bdbe0e4e8a311b85908c154f324d31a959ed
Ilya Payusov <ilya-payusov@mail.ru>
2026-02-04 13:36:33 +0300

    clear code

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, CustomLoginView, CustomLogoutView
+
+from .views import CustomLoginView, CustomLogoutView, RegisterView
 
 app_name = "users"
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,34 +2,36 @@ from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
 from .forms import CustomUserCreationForm, LoginForm
-from django.shortcuts import redirect
 
 
 class CustomLoginView(LoginView):
     form_class = LoginForm
-    template_name = 'users/login.html'
-    success_url = reverse_lazy('catalog:home')
+    template_name = "users/login.html"
+    success_url = reverse_lazy("catalog:home")
 
     def get_success_url(self):
         return self.success_url
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Вход'
+        context["title"] = "Вход"
         return context
 
 
 class CustomLogoutView(LogoutView):
-    template_name = 'users/logout.html'
+    template_name = "users/logout.html"
     next_page = None
+
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'users/registration.html'
-    success_url = reverse_lazy('catalog:home')
+    template_name = "users/registration.html"
+    success_url = reverse_lazy("catalog:home")
 
     def form_valid(self, form):
         # Сохраняем пользователя
@@ -37,8 +39,8 @@ class RegisterView(CreateView):
 
         # Отправляем приветственное письмо
         send_mail(
-            'Моей любимой женушке',
-            f'Здравствуйте, {user.username}! Спасибо за регистрацию.',
+            "Моей любимой женушке",
+            f"Здравствуйте, {user.username}! Спасибо за регистрацию.",
             settings.DEFAULT_FROM_EMAIL,
             [user.email],
             fail_silently=False,
@@ -51,6 +53,5 @@ class RegisterView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Регистрация'
+        context["title"] = "Регистрация"
         return context
-
